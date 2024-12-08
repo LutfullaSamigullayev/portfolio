1c654c3619f8d4aff6676466470ff48ddceb3ef7
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-08 11:05:25 +0500

    link edit

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,24 +3,23 @@ import { PortfolioItems } from "@/types";
 export const portfolioItems: PortfolioItems[] = [
   {
     id: 1,
-    link: "https://mexanik.vercel.app/",
+    link: "#",
     title: "Online fashion store - Homepage",
     desc: "React, Sass",
-    image: '/portfolioImage/portfolio1.jpg'
+    image: "/portfolioImage/portfolio1.jpg",
   },
   {
     id: 2,
-    link: "https://mexanik.vercel.app/",
+    link: "#",
     title: "Reebok Store - Concept",
     desc: "Nextjs, Tailwind",
-    image: '/portfolioImage/portfolio2.jpg'
+    image: "/portfolioImage/portfolio2.jpg",
   },
   {
     id: 3,
-    link: "https://mexanik.vercel.app/",
+    link: "#",
     title: "Braun Landing Page - Concept",
     desc: "React, Tailwind, TypeScript",
-    image: '/portfolioImage/portfolio3.jpg'
+    image: "/portfolioImage/portfolio3.jpg",
   },
-
-]
+];
